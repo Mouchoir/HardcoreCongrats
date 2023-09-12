ce8bf8f0579be29c4a1999ecd41180bdf51c3862
Mouchoir <github@mouchoir.mozmail.com>
2023-09-12 05:43:26 +0200

    Added pruning Pruning automatically removes a player from the list of players to congratulate after a certain amount of time has passed.

--- HardcoreCongrats.lua
+++ HardcoreCongrats.lua
@@ -1,22 +1,24 @@
 -- Database
 if not HardcoreCongratsDB then
     HardcoreCongratsDB = {
-    selectedMessageIndex = 1,  -- Default to the first message
-    isRandom = false,          -- Default to not random
-    serverMessage = "Awaiting for someone to reach level 60..." -- Default server message
-}
+        selectedMessageIndex = 1,
+        isRandom = false,
+        serverMessage = "Awaiting for someone to reach level 60...",
+        memoryDuration = 60 -- Default to 1 minute (600 seconds)
+    }
 end
 
 local button
-local congratulatedPlayers = {} -- Store players that have already been congratulated
+local pendingPlayers = {} -- Store players that will be congratulated
 local lastPlayer -- This variable will store the name of the last player to reach level 60
+local playerNameLabel
 
 -- List of congratulatory messages
 local congratsMessages = {
     "GG!",
     "Well done!",
     "Yeah you're 60!!",
-	"w00t",
+    "w00t",
     "Another hero survived"
 }
 
@@ -43,8 +45,6 @@ lastPlayerLabel:SetText(HardcoreCongratsLocalization[GetLocale()]["Last player t
 local lastPlayerValue = panel:CreateFontString("HardcoreCongratsConfigPanelLastPlayer", "ARTWORK", "GameFontWhite")
 lastPlayerValue:SetPoint("TOPLEFT", lastPlayerLabel, "BOTTOMLEFT", 0, -10)
 
-
-
 -- Add titles			 
 local messageTitle = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
 messageTitle:SetPoint("TOPLEFT", lastPlayerValue, "BOTTOMLEFT", 0, -20)
@@ -59,8 +59,32 @@ randomCheckbox:SetPoint("TOPLEFT", randomTitle, "BOTTOMLEFT", 0, -15)
 randomCheckbox.tooltip = "Randomly pick a message"
 getglobal(randomCheckbox:GetName() .. 'Text'):SetText("Random")
 
+local rememberTimeTitle = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+rememberTimeTitle:SetPoint("TOPLEFT", randomCheckbox, "BOTTOMLEFT", 0, -20)
+rememberTimeTitle:SetText(HardcoreCongratsLocalization[GetLocale()]["Remember"] or "Remember a player for:")
+
+-- Dropdown for memory duration
+local memoryDurations = {60, 120, 300} -- Durations in seconds
+local memoryDurationDropdown = CreateFrame("Frame", "HardcoreCongratsMemoryDurationDropdown", panel, "UIDropDownMenuTemplate")
+memoryDurationDropdown:SetPoint("TOPLEFT", rememberTimeTitle, "BOTTOMLEFT", -16, -30)
+UIDropDownMenu_SetWidth(memoryDurationDropdown, 150)
+UIDropDownMenu_SetText(memoryDurationDropdown, HardcoreCongratsDB.memoryDuration / 60 .. " minutes")
+
+UIDropDownMenu_Initialize(memoryDurationDropdown, function(self, level)
+    for _, duration in ipairs(memoryDurations) do
+        local info = UIDropDownMenu_CreateInfo()
+        info.text = (duration / 60) .. " " .. (HardcoreCongratsLocalization[GetLocale()]["Minutes"] or "minutes")
+        info.value = duration
+        info.func = function()
+            HardcoreCongratsDB.memoryDuration = duration
+            UIDropDownMenu_SetText(memoryDurationDropdown, info.text)
+        end
+        UIDropDownMenu_AddButton(info)
+    end
+end)
+
 local serverMessageTitle = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-serverMessageTitle:SetPoint("TOPLEFT", randomCheckbox, "BOTTOMLEFT", 0, -20)
+serverMessageTitle:SetPoint("TOPLEFT", memoryDurationDropdown, "BOTTOMLEFT", 16, -20)
 serverMessageTitle:SetText(HardcoreCongratsLocalization[GetLocale()]["Event server message"] or "Event server message")
 
 --Create an InputBox
@@ -82,7 +106,7 @@ localizationNoteLabel:SetJustifyH("LEFT")  -- Align text to the left
 localizationNoteLabel:SetPoint("TOPLEFT", localeOutput, "BOTTOMLEFT", 0, -20)
 localizationNoteLabel:SetText(HardcoreCongratsLocalization[GetLocale()]["Localization Note"] or "Copy/paste this to create the localization file in your own language since Blizzard sometimes uses special characters such as non-breakable spaces. Then replace the character's name with (.-).")
 
--- Function to get a message							
+
 local function getCongratsMessage()
     if randomCheckbox:GetChecked() then
         return congratsMessages[math.random(1, #congratsMessages)]
@@ -90,42 +114,105 @@ local function getCongratsMessage()
         return selectedMessage
     end
 end
-  
 
--- Update the sendCongratulation function to use the chosen message																   
-local function sendCongratulation(playerName)
-    if congratulatedPlayers[playerName] then
-        DEFAULT_CHAT_FRAME:AddMessage("You have already congratulated this player.")
-        return
+
+local function updateButtonAndNameLabelVisibility() --Handles "Congratulate and player labed visibility
+    if #pendingPlayers > 0 then
+        lastPlayer = pendingPlayers[#pendingPlayers].name
+        playerNameLabel:SetText(lastPlayer)  -- Set the name text
+        playerNameLabel:Show()  -- Explicitly show the name label again
+        button:Show()
+    else
+        lastPlayer = nil  -- Reset lastPlayer
+        playerNameLabel:SetText("")  -- Clear the name text
+        button:Hide()
+        playerNameLabel:Hide()  -- Hide the name label
     end
+end
+
+
+local function prunependingPlayers()
+    local currentTime = time()
+    for i = #pendingPlayers, 1, -1 do -- Looping backwards to safely remove elements
+        local playerInfo = pendingPlayers[i]
+        if currentTime - playerInfo.timestamp > HardcoreCongratsDB.memoryDuration then
+            DEFAULT_CHAT_FRAME:AddMessage("Player " .. playerInfo.name .. " has been removed from the list due to time exceeding.")
+            table.remove(pendingPlayers, i)
+        end
+    end
+    updateButtonAndNameLabelVisibility()
+end
+
+
+
+local function sendCongratulation(playerName)
+    local playerIndexToRemove
+    for index, playerInfo in ipairs(pendingPlayers) do
+        if playerInfo.name == playerName then
+            playerIndexToRemove = index
+            break
+        end
+    end
+    
     local message = getCongratsMessage()
     SendChatMessage(message, "WHISPER", nil, playerName)
-    congratulatedPlayers[playerName] = true
+    
+    -- Remove the player from the list after congratulating them
+    if playerIndexToRemove then
+        table.remove(pendingPlayers, playerIndexToRemove)
+        DEFAULT_CHAT_FRAME:AddMessage("Player " .. playerName .. " has been congratulated and removed from the list.")
+    end
+    
+    -- Check if there's another player to congratulate
+    updateButtonAndNameLabelVisibility()
+    prunependingPlayers()
 end
 
 
--- Event handling function						  
+
 local function onEvent(self, event, msg)
-    -- START TEST BUTTON LOGIC: This line is specifically for the test button and can be removed later.
-    DEFAULT_CHAT_FRAME:AddMessage("Received server message (Test Mode): " .. msg)
+    -- DEBUG: Print the received server message
+    DEFAULT_CHAT_FRAME:AddMessage("Received server message: " .. msg)
     -- END TEST BUTTON LOGIC
 
-    local localeMessage = HardcoreCongratsLocalization[GetLocale()].alert:gsub(" ", " ")  -- both are non-breaking spaces    
-    local playerName = string.match(msg, HardcoreCongratsLocalization[GetLocale()].alert)
+    local localeMessage = HardcoreCongratsLocalization[GetLocale()].alert -- Replace non-breaking spaces with regular spaces
+    local playerName = string.match(msg, localeMessage)
+
     if playerName then
-        -- START TEST BUTTON LOGIC: This line is specifically for the test button and can be removed later.
-        DEFAULT_CHAT_FRAME:AddMessage("Extracted Player Name (Test Mode): " .. playerName)
+         -- DEBUG: Print the extracted player name
+         DEFAULT_CHAT_FRAME:AddMessage("Extracted Player Name: " .. playerName)
         -- END TEST BUTTON LOGIC
 
-        lastPlayer = playerName
-        button:Show()
-        localeOutput:SetText(msg)  -- Update the EditBox with the extracted server message
-        HardcoreCongratsDB.serverMessage = msg;  -- Save the server message
+        local playerExists = false
+        for _, playerInfo in ipairs(pendingPlayers) do
+            if playerInfo.name == playerName then
+                playerInfo.congratulated = true
+                playerExists = true
+                break
+            end
+        end
+        
+        if not playerExists then
+            table.insert(pendingPlayers, {name = playerName, timestamp = time(), congratulated = false})
+            updateButtonAndNameLabelVisibility()
+        end        
+        
+        updateButtonAndNameLabelVisibility()
+        
     else
-        localeOutput:SetText("Awaiting for someone to reach level 60...")  -- Reset to the placeholder text if not the expected message
+         -- DEBUG: If player extraction failed, print a debug message
+         DEFAULT_CHAT_FRAME:AddMessage("Failed to extract player name from server message.")
+        lastPlayer = playerName
+        if msg == HardcoreCongratsDB.serverMessage then
+            button:Show()
+            localeOutput:SetText(msg)  -- Update the EditBox with the extracted server message
+            HardcoreCongratsDB.serverMessage = msg;  -- Save the server message
+        else
+            localeOutput:SetText("Awaiting for someone to reach level 60...")
+        end
+        playerNameLabel:Hide()  -- Hide the name label when the button is hidden
     end
 end
-
 
 -- Button to send the congratulation									
 button = CreateFrame("Button", "HardcoreCongratsButton", UIParent, "UIPanelButtonTemplate")
@@ -134,20 +221,17 @@ button:SetSize(150, 30)
 button:SetText("Congratulate!")
 button:Hide()
 button:SetScript("OnClick", function() 
-    if lastPlayer then
+    if #pendingPlayers > 0 then
+        lastPlayer = pendingPlayers[#pendingPlayers].name
         sendCongratulation(lastPlayer)
-        button:Hide() -- Hide the button after clicking
     end
 end)
-
 
 -- Register for the chat message event									  
 local f = CreateFrame("Frame")
 f:RegisterEvent("CHAT_MSG_SYSTEM")
 f:SetScript("OnEvent", onEvent)
 
-
--- Now, let's define the uncheckAllExcept function
 local function uncheckAllExcept(exceptIndex, touchRandomCheckbox)
     for i = 1, #congratsMessages + 1 do
         if i ~= exceptIndex then
@@ -193,8 +277,6 @@ for i, message in ipairs(congratsMessages) do
     end
 end
 
-
-
 randomCheckbox:SetScript("OnClick", function(self)
     if self:GetChecked() then
         uncheckAllExcept(#congratsMessages + 1) -- Uncheck all the message checkboxes
@@ -206,8 +288,6 @@ randomCheckbox:SetScript("OnClick", function(self)
     end
     HardcoreCongratsDB.isRandom = self:GetChecked()
 end)
-
-
 
 -- Set the checkbox state based on the saved setting
 _G["HardcoreCongratsCheckbox" .. HardcoreCongratsDB.selectedMessageIndex]:SetChecked(true)
@@ -264,6 +344,26 @@ testButton:SetSize(100, 30)
 testButton:SetPoint("TOPLEFT", 100, -100)
 testButton:SetText("TEST!") 
 
+-- Create a FontString above the button
+playerNameLabel = button:CreateFontString(nil, "OVERLAY", "GameFontWhite")
+playerNameLabel:SetPoint("BOTTOM", button, "TOP", 0, 5)  -- Position it above the button
+
+-- Create a backdrop for the FontString
+local backdrop = {
+    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",  -- Using a default texture
+    edgeSize = 16,
+    insets = { left = 4, right = 4, top = 4, bottom = 4 },
+}
+
+-- Create a frame to serve as the backdrop
+local backdropFrame = CreateFrame("Frame", nil, button, "BackdropTemplate")
+backdropFrame:SetSize(button:GetWidth(), 30)  -- Set the size you want
+backdropFrame:SetPoint("BOTTOM", button, "TOP", 0, 5)  -- Position it above the button
+
+-- Position the playerNameLabel on top of this frame
+playerNameLabel:SetPoint("CENTER", backdropFrame, "CENTER")
+
+
 testButton:SetScript("OnClick", function()
     -- Get random player name                            
     local playerName = getRandomPlayer()
@@ -278,6 +378,43 @@ testButton:SetScript("OnClick", function()
 
     -- Trigger onEvent() with mock event                                       
     onEvent(nil, nil, testEvent)
+
+    -- Display all registered player names
+    DEFAULT_CHAT_FRAME:AddMessage("Liste des pseudos enregistrés:")
+    for _, playerInfo in ipairs(pendingPlayers) do
+        DEFAULT_CHAT_FRAME:AddMessage(playerInfo.name)
+    end
 end)
 
+-- Register for the chat message event									  
+local f = CreateFrame("Frame")
+f:RegisterEvent("CHAT_MSG_SYSTEM")
+f:SetScript("OnEvent", onEvent)
+
+-- Call prunependingPlayers periodically, e.g., every 10 seconds
+C_Timer.NewTicker(10, prunependingPlayers)
 -- END OF TEST BUTTON LOGIC
+
+-- START OF THE SLASH COMMAND
+-- Register a slash command
+SLASH_HARDWARECONGRATS1 = "/hccongrats"
+SlashCmdList["HARDWARECONGRATS"] = function(msg)
+    if msg == "list" then
+        -- Check if there are any players in the list
+        if #pendingPlayers == 0 then
+            DEFAULT_CHAT_FRAME:AddMessage("No players are awaiting congratulations.")
+            return
+        end
+        
+        -- Display each player that is awaiting congratulations
+        DEFAULT_CHAT_FRAME:AddMessage("Players awaiting congratulations:")
+        for _, playerInfo in ipairs(pendingPlayers) do
+            if not playerInfo.congratulated then
+                DEFAULT_CHAT_FRAME:AddMessage(playerInfo.name)
+            end
+        end
+    else
+        DEFAULT_CHAT_FRAME:AddMessage("Usage: /hccongrats list - Displays list of players awaiting congratulations.")
+    end
+end
+-- END OF THE SLASH COMMAND
